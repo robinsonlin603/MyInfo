8f56c27ae22e15b834cc92c53fb4a708c7cb9bc8
robinson lin <am810603@gmail.com>
2022-07-24 11:01:24 +0800

    first push

---
+++ components/Layout.js
@@ -0,0 +1,5 @@
+const Layout = () => {
+  return <div className="content"></div>;
+};
+
+export default Layout;
---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">Robinson</div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Projects</a>
+    </nav>
+  );
+};
+
+export default Navbar;
+<nav>
+  <div className="logo">Robinson</div>
+  <a>Home</a>
+  <a>About</a>
+  <a></a>
+</nav>;
